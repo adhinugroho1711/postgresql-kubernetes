74e5605ad3f58f49f8b87bfb7203d404627dc2d8
P Adhi Nugroho <phenom1711@gmail.com>
2019-10-07 12:41:34 +0700

    Update install-postgresql.sh

--- install-postgresql.sh
+++ install-postgresql.sh
@@ -1,2 +1,2 @@
 kubectl create -f postgre-pvc.yaml
-helm install --name postgresql -f values-production.yaml --set postgresqlPassword=jateng001 --set replication.password=jateng001 --set postgresqlDatabase=master stable/postgresql
+helm install --name postgresql --namespace postgresql -f values-production.yaml --set postgresqlPassword=jateng001 --set replication.password=jateng001 --set postgresqlDatabase=master stable/postgresql
